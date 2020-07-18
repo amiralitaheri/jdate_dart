5a9cf3b09d2aeacba06b61fd2ecbcbc0ab73a713
amiralitaheri <amiralitaheri64@gmail.com>
2020-07-19 00:04:58 +0430

    fixed warnings and added todo
    Signed-off-by: amiralitaheri <amiralitaheri64@gmail.com>

--- lib/src/jdate_base.dart
+++ lib/src/jdate_base.dart
@@ -795,37 +795,37 @@ class JDate implements Comparable<JDate> {
       _microsecondsSinceEpoch - other.microsecondsSinceEpoch;
 
   static String _fourDigits(int n) {
-    int absN = n.abs();
-    String sign = n < 0 ? "-" : "";
-    if (absN >= 1000) return "$n";
-    if (absN >= 100) return "${sign}0$absN";
-    if (absN >= 10) return "${sign}00$absN";
-    return "${sign}000$absN";
+    var absN = n.abs();
+    var sign = n < 0 ? '-' : '';
+    if (absN >= 1000) return '$n';
+    if (absN >= 100) return '${sign}0$absN';
+    if (absN >= 10) return '${sign}00$absN';
+    return '${sign}000$absN';
   }
 
   static String _sixDigits(int n) {
     assert(n < -9999 || n > 9999);
-    int absN = n.abs();
-    String sign = n < 0 ? "-" : "+";
-    if (absN >= 100000) return "$sign$absN";
-    return "${sign}0$absN";
+    var absN = n.abs();
+    var sign = n < 0 ? '-' : '+';
+    if (absN >= 100000) return '$sign$absN';
+    return '${sign}0$absN';
   }
 
   static String _threeDigits(int n) {
-    if (n >= 100) return "${n}";
-    if (n >= 10) return "0${n}";
-    return "00${n}";
+    if (n >= 100) return '${n}';
+    if (n >= 10) return '0${n}';
+    return '00${n}';
   }
 
   static String _twoDigits(int n) {
-    if (n >= 10) return "${n}";
-    return "0${n}";
+    if (n >= 10) return '${n}';
+    return '0${n}';
   }
 
   /// Returns an ISO-8601 full-precision extended format representation.
   ///
   /// The format is `yyyy-MM-ddTHH:mm:ss.mmmuuuZ` for UTC time, and
-  /// `yyyy-MM-ddTHH:mm:ss.mmmuuu` (no trailing "Z") for local/non-UTC time,
+  /// `yyyy-MM-ddTHH:mm:ss.mmmuuu` (no trailing 'Z') for local/non-UTC time,
   /// where:
   ///
   /// * `yyyy` is a, possibly negative, four digit representation of the year,
@@ -890,3 +890,6 @@ class JDate implements Comparable<JDate> {
     return this;
   }
 }
+
+//todo: remove asserts
+//todo: fix js problem
